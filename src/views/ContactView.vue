9c06b5814d65f0a4cc1c451c1b5de91cc6255493
unknown <mutungaeric839@gmail.com>
2023-06-16 12:41:25 +0300

    posting error handling

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -9,7 +9,10 @@
             <input v-model="newContact.location" placeholder="Location" required>
             <button type="submit">Create</button>
         </form>
-
+        <div v-if="message" class="success-message">{{ message }}</div>
+        <div v-if="errors" class="error-message">
+            <div v-for="(value, key, index) in errors" :key="index">{{ `${key}: ${value}` }}</div>
+        </div>
         <h1>Contacts</h1>
         <div v-for="contact in contacts" :key="contact.id">
             <h2>{{contact.name}}</h2>
@@ -35,23 +38,32 @@ export default {
                 phone: '',
                 email: '',
                 location: ''
-            }
+            },
+            errors: {}, // store the validation errors
+            message: '' // store the success message
         }
     },
     methods: {
         submitForm() {
             axios.post('http://localhost:8000/api/contacts', this.newContact)
                 .then(response => {
-                    this.contacts.push(response.data);
+                    this.contacts.push(response.data.contact);
                     this.newContact = { name: '', phone: '', email: '', location: '' };
+                    this.message = response.data.message; // show success message
+                    this.clearMessage();
                 })
+                .catch(error => {
+                    if (error.response && error.response.status === 400) {
+                        // The request was made and the server responded with a status code
+                        // that falls out of the range of 2xx
+                        console.log(error.response.data);
+                        // Here error.response.data contains the validation errors
+                        this.errors = error.response.data;
+                        this.clearMessage();
+                    }
+                });
         },
         getContacts() {
-            const userId = localStorage.getItem('user_id');
-            if (!userId) {
-                alert('You must be logged in to view contacts.');
-                return;
-            }
             axios.get('http://localhost:8000/api/contacts')
                 .then(response => {
                     this.contacts = response.data;
@@ -68,6 +80,12 @@ export default {
                 .then(response => {
                     this.getContacts();
                 })
+        },
+        clearMessage() {
+            setTimeout(() => {
+                this.errors = {};
+                this.message = '';
+            }, 3000);
         }
     },
     created() {
@@ -78,3 +96,13 @@ export default {
     },
 }
 </script>
+
+<style>
+.error-message {
+    color: red;
+}
+
+.success-message {
+    color: green;
+}
+</style>
