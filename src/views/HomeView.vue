da726be8ba587bf97a996d6ee7885a234352191a
unknown <mutungaeric839@gmail.com>
2023-06-25 17:30:02 +0300

    home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,60 +1,59 @@
 <template>
-<UserNavigation/>
- <div class="homepage">
-        <h1>Welcome, {{ user.name }}</h1>
-        <p>Here are some resources you might find helpful:</p>
-        <ul>
-            <li v-for="resource in resources" :key="resource.id">
-                <a :href="resource.url">{{ resource.name }}</a>
-            </li>
-        </ul>
-    </div>
-</template>
+    <UserNavigation/>
+     <div class="homepage">
+            <h1>Welcome, {{ user.name }}</h1>
 
-<script>
-import UserNavigation from '../views/UserNavigation.vue'
-
-export default {
-    data() {
-        return {
-            user: {
-                name: 'John Doe', // replace with actual user data
-            },
-            resources: [
-                { id: 1, name: 'Resource 1', url: '#' },
-                { id: 2, name: 'Resource 2', url: '#' },
-                // add more resources as needed
-            ],
-        }
-    },
-    components: {
-        UserNavigation,
-    },
-    beforeMount() {
-        const userId = localStorage.getItem('user_id');
-        console.log('User ID:', userId);  // Print user ID to console
-
-        if (!userId) {
-            // Redirect to login page if user id is not present
-            this.$router.push('/login');
-        }
-        else {
-            // Load user data from API, etc.
-            // this.loadUserData(userId);
-        }
-    },
-    methods: {
-        loadUserData(userId) {
-            // Make API call to load user data, etc.
+        </div>
+    </template>
+    
+    <script>
+    import axios from 'axios'
+    import UserNavigation from '../views/UserNavigation.vue'
+    
+    export default {
+        data() {
+            return {
+                user: {
+                    name: '', // will be replaced with actual user data
+                },
+            }
+        },
+        components: {
+            UserNavigation,
+        },
+        beforeMount() {
+            const userId = localStorage.getItem('user_id');
+            console.log('User ID:', userId);  // Print user ID to console
+    
+            if (!userId) {
+                // Redirect to login page if user id is not present
+                this.$router.push('/login');
+            }
+            else {
+                this.loadUserData(userId);
+            }
+        },
+        methods: {
+            loadUserData(userId) {
+                axios.get(`http://localhost:8000/api/users/${userId}`)
+                    .then(response => {
+                        // assuming response.data has the user data
+                        this.user.name = response.data.name;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        // handle error
+                    });
+            }
         }
     }
-}
-</script>
-
-<style scoped>
-.homepage {
-    width: 80%;
-    margin: auto;
-    padding: 20px;
-}
-</style>
+    </script>
+    
+    <style scoped>
+    .homepage {
+        width: 80%;
+        margin: auto;
+        padding: 20px;
+    }
+    </style>
+    
