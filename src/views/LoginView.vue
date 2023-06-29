01a1d796edc6795402e1deeb502f72efaf04268c
unknown <mutungaeric839@gmail.com>
2023-06-29 15:23:08 +0300

    styling loging  page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="body-wrapper">
     <div class="container">
       <h1>Login</h1>
       <form @submit.prevent="login">
@@ -6,7 +7,7 @@
           <label for="email">Email</label>
           <input type="email" id="email" v-model="email" required>
         </div>
-  
+
         <div class="input-group">
           <label for="password">Password</label>
           <input type="password" id="password" v-model="password" required>
@@ -17,79 +18,105 @@
 
         <button class="submit-btn" type="submit">Login</button>
       </form>
-      <p class="register-link">If you are not a member, <router-link to="/register">register here</router-link>.</p>
+      <p class="register-link">Not a member yet? <router-link to="/register">Sign up here</router-link>.</p>
     </div>
+  </div>
 </template>
 
-  
-  <style>
+<style scoped>
 .container {
-  width: 100%;
-  max-width: 330px;
-  padding: 15px;
-  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+  height: 100vh;
+  margin: 0 auto;
+  padding: 40px;
+  text-align: center;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 8px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
 }
 
 h1 {
-  text-align: center;
+  font-size: 28px;
   margin-bottom: 30px;
+  color: #333333;
 }
 
 .input-group {
   margin-bottom: 20px;
+  text-align: left;
 }
 
 .input-group label {
   display: block;
+  font-size: 16px;
   margin-bottom: 10px;
+  color: #333333;
 }
 
 .input-group input {
   width: 100%;
-  padding: 10px;
-  border: 1px solid #ddd;
+  padding: 12px;
+  font-size: 16px;
+  border: 1px solid #dddddd;
   border-radius: 4px;
 }
 
 .submit-btn {
-  display: block;
-  width: 100%;
-  padding: 10px;
-  border: none;
-  color: white;
-  background-color: #007bff;
-  border-radius: 4px;
+  display: inline-block;
+  padding: 12px 24px;
+  font-size: 18px;
+  font-weight: bold;
   cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  color: #ffffff;
+  background-color: #4CAF50; /* Green background */
+  border: none;
+  border-radius: 4px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.3s ease-in-out;
 }
 
 .submit-btn:hover {
-  background-color: #0056b3;
+  background-color: #45a049; /* Darker green on hover */
 }
 
 .register-link {
-  text-align: center;
+  font-size: 14px;
   margin-top: 20px;
+  color: #666666;
 }
+
+.register-link a {
+  color: #007bff;
+  text-decoration: none;
+}
+
 .error {
   color: red;
   margin-top: 10px;
 }
 </style>
 
-  <script>
-  import axios from 'axios'
-  
-  export default {
-    data() {
-      return {
-        email: '',
-        password: '',
-        errorMessage: '',  // to store the error message
-      }
-    },
-    methods: {
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      errorMessage: '', // to store the error message
+    }
+  },
+  methods: {
     async login() {
-      this.errorMessage = '';  // clear previous error message
+      this.errorMessage = ''; // clear previous error message
       try {
         const response = await axios.post('http://localhost:8000/api/login', {
           email: this.email,
@@ -100,12 +127,12 @@ h1 {
         localStorage.setItem('user_id', response.data.user_id);
 
         // Check user_role and redirect
-        if(response.data.user_role === 'admin') {
+        if (response.data.user_role === 'admin') {
           this.$router.push('/adminpage');
         } else {
           this.$router.push('/homepage');
         }
-        
+
       } catch (error) {
         if (error.response) {
           // Show the error message from the response to the user
@@ -116,4 +143,11 @@ h1 {
   }
 }
 </script>
-  
+
+<style>
+.body-wrapper {
+  background-image: url('back.jpg');
+  background-size: cover;
+  background-position: center;
+}
+</style>
