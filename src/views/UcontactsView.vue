12f572cf277065a3d1bcb5045329310a851ca333
unknown <mutungaeric839@gmail.com>
2023-06-30 12:58:50 +0300

    contact page

--- src/views/UcontactsView.vue
+++ src/views/UcontactsView.vue
@@ -1,49 +1,75 @@
 <template>
     <UserNavigation/>
-     <div class="homepage">
-            <p>Contacts view:</p>
+    <div class="homepage">
+        <h1>Contacts</h1>
+        <input v-model="searchTerm" type="text" placeholder="Search by location...">
+        <div v-for="contact in filteredContacts" :key="contact.id">
+            <h2>{{ contact.name }}</h2>
+            <p>{{ contact.phone }}</p>
+            <p>{{ contact.email }}</p>
+            <p>{{ contact.location }}</p>
         </div>
-        <FooterView/>
+    </div>
+    <FooterView/>
+</template>
 
-    </template>
-    
-    <script>
-    import UserNavigation from '../views/UserNavigation.vue'
-    import FooterView from '../views/FooterView.vue'
+<script>
+import UserNavigation from '../views/UserNavigation.vue'
+import FooterView from '../views/FooterView.vue'
+import axios from 'axios';
 
-    export default {
-        
-        components: {
-            UserNavigation,
-            FooterView,
-
-        },
-        beforeMount() {
-            const userId = localStorage.getItem('user_id');
-            console.log('User ID:', userId);  // Print user ID to console
-    
-            if (!userId) {
-                // Redirect to login page if user id is not present
-                this.$router.push('/login');
-            }
-            else {
-                // Load user data from API, etc.
-                // this.loadUserData(userId);
-            }
-        },
-        methods: {
-            loadUserData(userId) {
-                // Make API call to load user data, etc.
-            }
+export default {
+    components: {
+        UserNavigation,
+        FooterView,
+    },
+    data() {
+        return {
+            contacts: [],
+            searchTerm: ''
         }
+    },
+    computed: {
+        filteredContacts() {
+            if (!this.searchTerm) {
+                return this.contacts;
+            }
+
+            return this.contacts.filter(contact =>
+                contact.location.toLowerCase().includes(this.searchTerm.toLowerCase())
+            );
+        }
+    },
+    beforeMount() {
+        const userId = localStorage.getItem('user_id');
+        console.log('User ID:', userId);  // Print user ID to console
+
+        if (!userId) {
+            // Redirect to login page if user id is not present
+            this.$router.push('/login');
+        }
+        else {
+            // Fetch contacts from API
+            this.fetchContacts();
+        }
+    },
+    methods: {
+        async fetchContacts() {
+            try {
+                const response = await axios.get('http://localhost:8000/api/contacts');
+                this.contacts = response.data;
+            } catch (error) {
+                console.error(error);
+            }
+        },
     }
-    </script>
-    
-    <style scoped>
-    .homepage {
-        width: 80%;
-        margin: auto;
-        padding: 20px;
-    }
-    </style>
-    
+}
+</script>
+
+<style scoped>
+.homepage {
+    width: 80%;
+    margin: auto;
+    padding: 20px;
+}
+</style>
